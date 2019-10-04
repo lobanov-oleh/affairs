c3036f749526601651febbd629faa4bc38fba156
Vladimir Plakhotnik <vn.plakhotnik@outlook.com>
2019-10-04 11:49:38 +0300

    Add Contribution model

--- src/models/Contribution.ts
+++ src/models/Contribution.ts
@@ -1,2 +1,32 @@
+import { Project } from "./Project";
+import { User } from "./User";
+
 export class Contribution {
+    private _Project: Project
+    private _User: User
+    private _description: string
+
+    get Project(): Project {
+        return this._Project
+    }
+
+    set Project(Project: Project) {
+        this._Project = Project
+    }
+
+    get User(): User {
+        return this._User
+    }
+
+    set User(User: User) {
+        this._User = User
+    }
+
+    get description(): string {
+        return this._description
+    }
+
+    set description(description: string) {
+        this._description = description
+    }
 }
