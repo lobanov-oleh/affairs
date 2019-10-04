3823624feb816eb9d07c7fdbe04504c40a5ee1df
Vladimir Plakhotnik <vn.plakhotnik@outlook.com>
2019-10-04 11:55:48 +0300

    Fixes

--- src/models/Contribution.ts
+++ src/models/Contribution.ts
@@ -1,5 +1,5 @@
-import { Project } from "./Project";
-import { User } from "./User";
+import { Project } from "./Project"
+import { User } from "./User"
 
 export class Contribution {
     private _Project: Project
