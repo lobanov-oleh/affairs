ca67178992c57498f24f459d8f8669540c7dd100
Vladimir Plakhotnik <vn.plakhotnik@outlook.com>
2019-10-04 12:21:53 +0300

    Add Offers and User models. Fix Project model

--- src/models/Offer.ts
+++ src/models/Offer.ts
@@ -1,2 +1,32 @@
+import { Contribution } from "./Contribution"
+import { User } from "./User"
+
 export class Offer {
+    private _Contribution: Contribution
+    private _User: User
+    private _amount: number
+
+    get Contribution(): Contribution {
+        return this._Contribution
+    }
+
+    set Contribution(Contribution: Contribution) {
+        this._Contribution = Contribution
+    }
+
+    get User(): User {
+        return this._User
+    }
+
+    set User(User: User) {
+        this._User = User
+    }
+
+    get amount(): number {
+        return this._amount
+    }
+
+    set amount(amount: number) {
+        this._amount = amount
+    }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,2 +1,9 @@
 export class User {
+    private _facebookUserID: string
+    get facebookUserID(): string {
+        return this._facebookUserID
+    }
+    set facebookUserID(facebookUserID: string) {
+        this._facebookUserID = facebookUserID
+    }
 }
