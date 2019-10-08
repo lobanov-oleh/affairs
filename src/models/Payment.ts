91e453afeae21b71c29fe20bdf462d9c5b026040
Vladimir Plakhotnik <vn.plakhotnik@outlook.com>
2019-10-08 13:02:53 +0300

    Implementation models (Acceptance, Obligation, Payments, Rejection)

--- src/models/Payment.ts
+++ src/models/Payment.ts
@@ -1,2 +1,23 @@
+import { Offer } from "./Offer";
+import { User } from "./User";
+
 export class Payment {
+    private _Offer: Offer
+    private _User: User
+
+    get Offer(): Offer {
+        return this._Offer
+    }
+
+    set Offer(Offer: Offer) {
+        this._Offer = Offer
+    }
+
+    get User(): User {
+        return this._User
+    }
+
+    set User(User: User) {
+        this._User = User
+    }
 }
